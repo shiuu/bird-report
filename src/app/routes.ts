420618a4169bf546f32b205200dc37fb1944c1a2
shiuu <shiuuken@gmail.com>
2018-05-13 19:48:46 +1000

    Added user module.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -16,5 +16,6 @@ export const appRoutes:Routes = [
   { path: 'reports/new', component: CreateReportComponent, canDeactivate: ['canDeactivateCreateReport'] },
   { path: 'reports', component: ReportListComponent, resolve: {reports: ReportListResolver}},
   { path: 'reports/:id', component: ReportDetailsComponent, resolve: {report: ReportResolver}},
-  { path: '', redirectTo: '/reports', pathMatch: 'full'}
+  { path: '', redirectTo: '/reports', pathMatch: 'full'},
+  { path: 'user', loadChildren: 'app/user/user.module#UserModule' }
 ]
